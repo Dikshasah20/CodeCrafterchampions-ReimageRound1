b30862fb4255bdc1abda26fb1f8e74c4733354b0
41chaitanya <chaitanya4141sharma@gmail.com>
2024-05-06 13:12:28 +0530

    feat: add loader spinner <chaitanyaSharma>

---
+++ script.js
@@ -0,0 +1,28 @@
+{//loader Spiner <By chaitanyaSharma>
+            const spinnerOverlay=document.createElement('div')   //spinner overlay create
+            spinnerOverlay.classList.add('spinner-wrapper')     //spinner overlay class
+            document.body.insertAdjacentElement("afterbegin", spinnerOverlay) // inert overlay after the body tag
+        // _____________________________________________________________________________________________________________________________________________________________________
+
+            const spinnerBox=document.createElement('div')    //spinner Box create
+            spinnerBox.classList.add('spinner-border','text-light')       //spinner Box class
+            spinnerBox.setAttribute('role','status')         //spinner Box role attribute
+            spinnerOverlay.appendChild(spinnerBox)         // inert spinner box after the overlay tag
+
+
+            //the loder will invesible after 1 sec
+            
+        window.addEventListener('load',()=>{
+
+                setTimeout(()=>{
+                    document.querySelector('.spinner-wrapper').style.opacity='0'
+                    this.setTimeout(function(event){
+                            document.querySelector('.spinner-wrapper').style.display='none'
+                        },500)
+                },1000)
+            
+
+
+        })
+}//end the loader section
+    
